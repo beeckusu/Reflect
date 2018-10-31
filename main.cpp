5463c925967f7d68a7067235f4116f755da243c3
beeckusu <gavinlu94@gmail.com>
2018-10-31 15:35:37 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+#include "main.h"
+
+
+int refresh_rate;
+
+
+int main() {
+
+}
+
+
+void tick() {
+
+}
+
+void refresh() {
+
+}
---
+++ module.h
@@ -0,0 +1,17 @@
+// main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+class Module
+{
+
+	//Private members
+
+
+	//Public members
+	public:
+		string name;
+
+};
+
+#endif
